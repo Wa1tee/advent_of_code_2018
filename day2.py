b3414ddde7ede6c2e177a30fd3077462df52498a
Wa1tee <antti.auranen@iki.fi>
2018-12-11 22:40:54 +0200

    Started working on day2

---
+++ day2.py
@@ -0,0 +1,23 @@
+# Advent of code 2018
+# Antti "Waitee" Auranen
+# Day 2
+def main():
+    inputs = []
+    doubles = 0
+    triples = 0
+    while(True):
+
+        s = input('> ')
+        if s.length() > 0:
+            inputs.append(s)
+            for i in s:
+                for x in s:
+                    if x == i:
+                        
+                
+        else
+            break
+
+    while(True):
+
+    return 0
