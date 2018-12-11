45078f52330911e3df2c4def8293bce6d26b7c87
Wa1tee <antti.auranen@iki.fi>
2018-12-11 22:10:41 +0200

    Day1 both challenges completed

--- day1.py
+++ day1.py
@@ -3,13 +3,41 @@
 
 def main():
     number = 0
+    numbers = []
+    frequencies = []
+    rounds = 0
     while(True):
         try:
             s = int(input('> '))
 
         except ValueError:
             print("Frequency is " , number)
-            return number
+            break
+            
+            
+
+        numbers.append(s)
         number += s
+        
+        for x in frequencies:
+            if x == number:
+                print("Frequency is " , number)
+                print("First frequency twice is ", x)
+                return 
+        frequencies.append(number)
+
+    while(True):
+        rounds += 1
+        print("Rounds passed: ", rounds)
+        for i in numbers:
+            
+            number += i
+            for x in frequencies:
+                if x == number:
+                    print("Frequency is " , number)
+                    print("First frequency twice is ", x)
+                    return 0
+            frequencies.append(number)
+    return 1
 
 main()
