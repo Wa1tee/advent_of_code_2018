f04e13beb51099c0fe66e026b82200df77a97ad2
Wa1tee <antti.auranen@iki.fi>
2018-12-12 23:18:41 +0200

    day 3 both challenges finished

--- day3.py
+++ day3.py
@@ -7,11 +7,19 @@ def main():
     row = []
     matrix = []
     overlap = 0
+    claims = []
+    idnumbers = []
     for x in range(1,1200):
         row = []
         for y in range(1,1200):
             row.append(0)
         matrix.append(row)
+
+    for x in range(1,1200):
+        row = []
+        for y in range(1,1200):
+            row.append(0)
+        claims.append(row)
 
     while(True):
         s = input("> ")
@@ -24,10 +32,15 @@ def main():
             width   = int( s[ s.find(": ") + 2 : s.find("x") ])
             height  = int( s[ s.find("x")  + 1 : ])
 
+            idnumbers.append(idno)
+            
             for i in range(left, left + width):
                 for j in range(top, top + height):
                     matrix[j][i] += 1
-
+                    if claims[j][i] > 0:
+                        idnumbers[claims[j][i]-1] = 0
+                        idnumbers[idno -1] = 0
+                    claims[j][i] = idno
 
         else:
             break
@@ -36,7 +49,9 @@ def main():
         for j in i:
             if j > 1:
                 overlap += 1
-
+    for i in idnumbers:
+        if i > 0:
+            print("Non-overlapping claim: ", i)
     print("Overlapping square inches: ", overlap)
 
     return 0
