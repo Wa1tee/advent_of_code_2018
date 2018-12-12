d70777aff15e4252c2f25dfd55fbc06fabe59bab
Wa1tee <antti.auranen@iki.fi>
2018-12-12 21:53:38 +0200

    Day 2 both challenges completed

--- day2.py
+++ day2.py
@@ -28,7 +28,7 @@ def main():
 
             double = False
             triple = False
-            # print(counts)
+            
             for j in counts:
                 if j == 2:
                     double = True
@@ -36,35 +36,28 @@ def main():
                     triple = True
             if double:
                 doubles += 1
-                print("Doubleadd: ", doubles)
             if triple:
                 triples += 1
-                print("Tripleadd: ", triples )
                 
         else:
             break
 
     checksum = doubles * triples
-    print("Doubles: ", doubles)
-    print("Triples: ", triples)
+
     print("Checksum is ", checksum)
 
     for i in range(0, len(inputs)):
         
         for j in range(0, len(inputs)) :
-            print("i = ", i)
-            print("j = ", j)
-
+            if i == j:
+                continue
             for k in range(0, inputlength-1) :
                 line1 = inputs[i]
                 line2 = inputs[j]
-                line1 = line1[:k] + " " + line1[k+1:] 
-                line2 = line2[:k] + " " + line2[k+1:] 
+                line1 = line1[:k] + line1[k+1:] 
+                line2 = line2[:k] + line2[k+1:] 
                 if line1 == line2:
-                    print("Line 1: ", inputs[i])
-                    print("Line 2: ", inputs[j])
-                    print("new   : ", line1)
-                    print(" ")
+                    
                     
                     checkedinputs.append(line1)
     if len(checkedinputs) > 0:
@@ -72,6 +65,7 @@ def main():
         print("ID found: ", checkedinputs[0])
     else:
         print("ID not found.")
+    
     return 0
 
 main()
