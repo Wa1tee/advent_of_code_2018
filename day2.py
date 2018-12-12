4437e6b13c761bbb57988b66d9c7e8c77952974e
Antti Auranen <antti.auranen@iki.fi>
2018-12-12 14:21:20 +0200

    Day2 first challenge complete

--- day2.py
+++ day2.py
@@ -3,21 +3,49 @@
 # Day 2
 def main():
     inputs = []
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
     doubles = 0
     triples = 0
+    counts = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    double = False
+    triple = False
+
+    
     while(True):
 
         s = input('> ')
-        if s.length() > 0:
+        if len(s) > 0:
             inputs.append(s)
+            amount = 0
+            counts = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
             for i in s:
-                for x in s:
-                    if x == i:
-                        
+                for x in range(0,26):
+                    if alphabet[x] == i:
+                        counts[x] += 1
+
+            double = False
+            triple = False
+            # print(counts)
+            for j in counts:
+                if j == 2:
+                    double = True
+                if j == 3:
+                    triple = True
+            if double:
+                doubles += 1
+                print("Doubleadd: ", doubles)
+            if triple:
+                triples += 1
+                print("Tripleadd: ", triples )
                 
-        else
+        else:
             break
 
-    while(True):
+    checksum = doubles * triples
+    print("Doubles: ", doubles)
+    print("Triples: ", triples)
+    print("Checksum is ", checksum)
 
     return 0
+
+main()
